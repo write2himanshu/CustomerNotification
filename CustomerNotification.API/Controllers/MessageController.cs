254b7871b3c789ede5b2d0d0f6078537e4e2543e
HS <write2himanshu@hotmail.com>
2021-02-21 21:28:06 +1100

    Initial check-in - source solution

---
+++ CustomerNotification.API/Controllers/MessageController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerNotification.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessageController : ControllerBase
+    {
+    } 
+}
---
+++ CustomerNotificaton.Services/MessagingService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CustomerNotificaton.Services
+{
+    public class MessagingService
+    {
+        public async Task SendMessageAsync(string customerId, string messageBody)
+        {
+            //this is a mock method and candidates don't need to chage this
+            await Task.Delay(1000);
+            Console.WriteLine($"sending customer id: {customerId}, the following message {messageBody}");
+        }
+    }
+}
